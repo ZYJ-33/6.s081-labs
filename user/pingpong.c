da7090aa5575c85c7cb769b978f87329a338d5fd
Ubuntu <ubuntu@ubuntu.lan>
2021-10-07 12:34:30 +0000

    finish pingpong lab

---
+++ user/pingpong.c
@@ -0,0 +1,29 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char* argv[])
+{
+   int p_fd[2];
+   int c_fd[2];
+   pipe(p_fd);
+   pipe(c_fd);
+
+   if(fork() == 0)
+   {
+	int pid = getpid();
+	char tmp = 'a';
+	read(p_fd[1], &tmp, 1);
+	printf("<%d>: receive ping", pid);
+	write(c_fd[0], &tmp, 1);
+	exit(0);
+   }
+   else
+   {
+	char tmp = 'a';
+	int pid = getpid();
+	write(p_fd[0], &tmp, 1);
+	read(c_fd[1], &tmp, 1);
+	printf("<%d>: receive pong", pid);
+	exit(0);
+   } 
+}
